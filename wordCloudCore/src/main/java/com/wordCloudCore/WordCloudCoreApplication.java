2c0cfff1fbaf151623040729022ae6f59946f68f
Markkus Koodi <markkus999@outlook.com>
2022-09-02 18:33:53 +0300

    Adding Word Cloud Core.

---
+++ wordCloudCore/src/main/java/com/wordCloudCore/WordCloudCoreApplication.java
@@ -0,0 +1,13 @@
+package com.wordCloudCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordCloudCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordCloudCoreApplication.class, args);
+	}
+
+}
---
+++ wordCloudCore/src/test/java/com/wordCloudCore/WordCloudCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordCloudCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCloudCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
